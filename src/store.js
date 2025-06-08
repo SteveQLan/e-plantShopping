0143b16f82198ab2554a1d02d703358aa28fc876
SteveQLan <lanqing8395@gmail.com>
2025-06-08 15:01:59 -0400

    New

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice.jsx';
+import cartReducer from './CreateSlice.jsx';
 
  c    // Create a Redux store using configureStore from Redux Toolkit
     const store = configureStore({
